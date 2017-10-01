4518f19439c3c141e43b9f1c6ab65b3f46ebbea5
iSm1le <ism1le@xaff.ru>
2017-10-02 02:25:53 +0300

    updated user api

--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,21 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-module.exports = mongoose.model('User', new mongoose.Schema({
-    username: { type: String, required: true },
+let UserSchema = new mongoose.Schema({
+    username: { type: String, unique: true, required: true },
     email: { type: String, unique: true, lowercase: true, trim: true, required: true },
     password: { type: String, required: true },
-    services: String
-}));
+    services: String,
+    created: { type: Date, default: Date.now }
+});
+
+
+/* eslint-disable func-names */
+UserSchema.methods.comparePassword = function(pass) {
+    return bcrypt.compareSync(pass, this.password);
+};
+/* eslint-enable func-names */
+
+let User = mongoose.model('User', UserSchema);
+
+module.exports = User;
--- app/routes/user_routes.js
+++ app/routes/user_routes.js
@@ -1,9 +1,11 @@
-const user = require('../models/user');
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 /* eslint-disable func-names, consistent-return */
 module.exports = function(app) {
     app.get('/user/:id', (req, res) => {
-        user.findOne({ _id: req.params.id }, (err, obj) => {
+        User.findOne({ _id: req.params.id }, (err, obj) => {
             if (err) {
                 res.send({ status: false, error: 'An error has occured' });
             } else {
@@ -13,7 +15,7 @@ module.exports = function(app) {
         });
     });
     app.put('/user/:id', (req, res) => {
-        user.findOneAndUpdate({ _id: req.params.id }, req.body, err => {
+        User.findOneAndUpdate({ _id: req.params.id }, req.body, err => {
             if (err) {
                 res.send({ status: false, error: 'An error has occured' });
             } else {
@@ -22,7 +24,7 @@ module.exports = function(app) {
         });
     });
     app.delete('/user/:id', (req, res) => {
-        user.findOneAndRemove({ _id: req.params.id }, err => {
+        User.findOneAndRemove({ _id: req.params.id }, err => {
             if (err) {
                 res.send({ status: false, error: 'An error has occured' });
             } else {
@@ -30,18 +32,47 @@ module.exports = function(app) {
             }
         });
     });
-    app.post('/user', (req, res) => {
-        const obj = new user(req.body); // eslint-disable-line new-cap
+    app.post('/user/register', (req, res) => {
+        const obj = new User(req.body); // eslint-disable-line new-cap
+        obj.password = bcrypt.hashSync(req.body.password, 10);
         obj.save((err, item) => {
             // 11000 is the code for duplicate key error
             if (err && err.code === 11000) {
-                res.sendStatus(400);
+                res.sendStatus(400).send({ message: 'User exists!' });
             }
             if (err) {
-                res.send({ status: false, error: 'An error has occured' });
+                res.sendStatus(400).send({ message: err });
             }
-            res.status(200).json(item);
+            item.password = undefined;
+            return res.status(200).json(item);
         });
+    });
+    app.post('/user/login', (req, res) => {
+        User.findOne({ email: req.body.email }, (err, obj) => {
+            if (err) throw err;
+            if (!obj) {
+                res.status(401).json({ message: 'Auth failed. User not found' });
+            } else if (obj) {
+                if (!obj.comparePassword(req.body.password)) {
+                    res.status(401).json({ message: 'Auth failed. Wrong password' });
+                } else {
+                    return res.json({
+                        token: jwt.sign({
+                            email: obj.email,
+                            username: obj.username,
+                            _id: obj._id
+                        }, 'RESTFULAPIs')
+                    });
+                }
+            }
+        });
+    });
+    app.post('/user/islogged', (req, res, next) => {
+        if (req.user) {
+            next(); // eslint-disable-line callback-return
+        } else {
+            return res.status(401).json({ message: 'Unauthorized user!' });
+        }
     });
 };
 /* eslint-enable func-names */
