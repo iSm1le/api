276c38597c6fb3ef2cdac587c0d86f64cb66967f
iSm1le <ism1le@xaff.ru>
2017-10-02 01:42:22 +0300

    mongo connection changed

--- api.js
+++ api.js
@@ -4,9 +4,17 @@ const bodyParser = require('body-parser');
 const db = require('./config/db');
 const app = express();
 const port = 8000;
+
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-mongoose.connect(db.url, { useMongoClient: true }, err => { // eslint-disable-line consistent-return
-    if (err) return console.log(err); // eslint-disable-line no-console
+
+mongoose.connect(db.url, { useMongoClient: true });
+const database = mongoose.connection;
+mongoose.Promise = global.Promise;
+
+database.on('error', console.error.bind(console, 'connection error:')); // eslint-disable-line no-console
+database.once('open', () => {
+    console.log('MongoDB connected!'); // eslint-disable-line no-console
     require('./app/routes')(app);
     app.listen(port, () => {
         console.log(`Live on ${port} port`); // eslint-disable-line no-console
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,9 @@
 const userRoutes = require('./user_routes');
 /* eslint-disable func-names */
-module.exports = function(app, db) {
+module.exports = function(app) {
     app.get('/', (req, res) => {
         res.redirect(301, '/docs');
     });
-    userRoutes(app, db);
+    userRoutes(app);
 };
 /* eslint-enable func-names */
