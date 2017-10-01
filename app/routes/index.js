f4ed3e0d5546df30034e183087258099a1a7aef9
iSm1le <ism1le@xaff.ru>
2017-10-02 00:52:36 +0300

    redirect from root

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,9 @@
 const userRoutes = require('./user_routes');
 /* eslint-disable func-names */
 module.exports = function(app, db) {
+    app.get('/', (req, res) => {
+        res.redirect(301, '/docs');
+    });
     userRoutes(app, db);
 };
 /* eslint-enable func-names */
