748a1a21d174450af4c6144c81d69a4e375eeb24
iSm1le <ism1le@xaff.ru>
2017-10-02 11:41:25 +0300

    empty body check

--- app/routes/user_routes.js
+++ app/routes/user_routes.js
@@ -4,57 +4,60 @@ const jwt = require('jsonwebtoken');
 
 /* eslint-disable func-names, consistent-return */
 module.exports = function(app) {
-    app.get('/user/:id', (req, res) => {
+    app.get('/user/id/:id', (req, res) => {
         User.findOne({ _id: req.params.id }, (err, obj) => {
             if (err) {
-                res.send({ status: false, error: 'An error has occured' });
+                return res.send({ status: false, error: 'An error has occured' });
             } else {
                 if (!obj) return res.send({ status: false, error: 'User not found' });
-                res.json(obj);
+                return res.json(obj);
             }
         });
     });
-    app.put('/user/:id', (req, res) => {
+    app.put('/user/id/:id', (req, res) => {
+        if (!req.body.email || !req.body.password) return res.status(400).json({ message: 'Empty body!' });
         User.findOneAndUpdate({ _id: req.params.id }, req.body, err => {
             if (err) {
-                res.send({ status: false, error: 'An error has occured' });
+                return res.send({ status: false, error: 'An error has occured' });
             } else {
-                res.sendStatus(200);
+                return res.sendStatus(200);
             }
         });
     });
-    app.delete('/user/:id', (req, res) => {
+    app.delete('/user/id/:id', (req, res) => {
         User.findOneAndRemove({ _id: req.params.id }, err => {
             if (err) {
-                res.send({ status: false, error: 'An error has occured' });
+                return res.send({ status: false, error: 'An error has occured' });
             } else {
-                res.sendStatus(200);
+                return res.sendStatus(200);
             }
         });
     });
     app.post('/user/register', (req, res) => {
+        if (!req.body.email || !req.body.password) return res.status(400).json({ message: 'Empty body!' });
         const obj = new User(req.body); // eslint-disable-line new-cap
         obj.password = bcrypt.hashSync(req.body.password, 10);
         obj.save((err, item) => {
             // 11000 is the code for duplicate key error
             if (err && err.code === 11000) {
-                res.sendStatus(400).send({ message: 'User exists!' });
+                return res.sendStatus(400).send({ message: 'User exists!' });
             }
             if (err) {
-                res.sendStatus(400).send({ message: err });
+                return res.sendStatus(400).send({ message: 'An error has occured' });
             }
             item.password = undefined;
             return res.status(200).json(item);
         });
     });
     app.post('/user/login', (req, res) => {
+        if (!req.body.email || !req.body.password) return res.status(400).json({ message: 'Empty body!' });
         User.findOne({ email: req.body.email }, (err, obj) => {
             if (err) throw err;
             if (!obj) {
-                res.status(401).json({ message: 'Auth failed. User not found' });
+                return res.status(401).json({ message: 'Auth failed. User not found' });
             } else if (obj) {
                 if (!obj.comparePassword(req.body.password)) {
-                    res.status(401).json({ message: 'Auth failed. Wrong password' });
+                    return res.status(401).json({ message: 'Auth failed. Wrong password' });
                 } else {
                     return res.json({
                         token: jwt.sign({
