8b41f6e2b7c13dd4ed442da08a5500e9e7f9a147
iSm1le <ism1le@xaff.ru>
2017-10-04 23:44:15 +0300

    Config update
    Signed-off-by: iSm1le <ism1le@xaff.ru>

--- server/api.ts
+++ server/api.ts
@@ -1,20 +1,19 @@
 import * as bodyParser from 'body-parser';
-import * as dotenv from 'dotenv';
 import * as express from 'express';
 import * as morgan from 'morgan';
 import * as mongoose from 'mongoose';
+const config = require('../config/config.json');
 
 import setRoutes from './app/routes';
 
 const app = express();
-dotenv.load({ path: '.env' });
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (config.port || 3000));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(morgan('dev'));
-mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true });
+mongoose.connect(config.dbUrl, { useMongoClient: true });
 const db = mongoose.connection;
 (<any>mongoose).Promise = global.Promise;
 
--- server/app/controllers/user.ts
+++ server/app/controllers/user.ts
@@ -1,8 +1,9 @@
-import * as dotenv from 'dotenv';
 import * as jwt from 'jsonwebtoken';
 
 import User from '../models/user';
 import BaseCtrl from './base';
+
+const config = require('../../../config/config');
 
 export default class UserCtrl extends BaseCtrl {
     model = User;
@@ -12,7 +13,7 @@ export default class UserCtrl extends BaseCtrl {
             if (!user) { return res.sendStatus(403); }
             user.comparePassword(req.body.password, (error, isMatch) => {
                 if (!isMatch) { return res.sendStatus(403); }
-                const token = jwt.sign({ user: user }, process.env.SECRET_TOKEN); // , { expiresIn: 10 } seconds
+                const token = jwt.sign({ user: user }, config.sToken); // , { expiresIn: 10 } seconds
                 res.status(200).json({ token: token });
             });
         });
