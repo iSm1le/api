281908bffa87a6b444076a23ba3a30e307288eeb
iSm1le <ism1le@xaff.ru>
2017-10-21 17:28:27 +0300

    Indent fix
    Signed-off-by: Mikhail K <ism1le@xaff.ru>

--- server/app/controllers/user.ts
+++ server/app/controllers/user.ts
@@ -5,17 +5,21 @@ import BaseCtrl from './base';
 const config = require('../../../config/config');
 
 export default class UserCtrl extends BaseCtrl {
-    model = User;
+  model = User;
 
-    login = (req, res) => {
-        this.model.findOne({ email: req.body.email }, (err, user) => {
-            if (!user) { return res.sendStatus(403); }
-            user.comparePassword(req.body.password, (error, isMatch) => {
-                if (!isMatch) { return res.sendStatus(403); }
-              const token = jwt.sign({ user: user }, config.sToken, { expiresInMinutes: 1440 }); // , { expiresIn: 10 } seconds
-                res.status(200).json({ token: token });
-            });
-        });
-    }
+  login = (req, res) => {
+    this.model.findOne({ email: req.body.email }, (err, user) => {
+      if (!user) {
+        return res.sendStatus(403);
+      }
+      user.comparePassword(req.body.password, (error, isMatch) => {
+        if (!isMatch) {
+          return res.sendStatus(403);
+        }
+        const token = jwt.sign({ user: user }, config.sToken, { expiresInMinutes: 1440 }); // , { expiresIn: 10 } seconds
+        res.status(200).json({ token: token });
+      });
+    });
+  }
 
 }
--- server/app/telegram/telegramapp.ts
+++ server/app/telegram/telegramapp.ts
@@ -4,7 +4,7 @@ const config = require('../../../config/config.json');
 const telegrafApp = new Telegraf(config.TELEGRAM_TOKEN);
 
 telegrafApp.telegram.getMe().then((botInfo) => {
-    telegrafApp.options.username = botInfo.username;
+  telegrafApp.options.username = botInfo.username;
 });
 
 telegrafApp.telegram.setWebhook(config.TELEGRAM_WEBHOOK.URL + '/' + config.TELEGRAM_WEBHOOK.SECRET_PATH, config.TELEGRAM_WEBHOOK.MAXCONN);
